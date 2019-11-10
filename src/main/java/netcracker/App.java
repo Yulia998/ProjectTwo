a51cef24784d663f573d702b8c75a64d1246dcc0
Yulia <mykhailenkoyulia98@gmail.com>
2019-11-10 16:08:24 +0200

    Logging was implemented

--- src/main/java/netcracker/App.java
+++ src/main/java/netcracker/App.java
@@ -2,14 +2,17 @@ package netcracker;
 
 import netcracker.exceptions.HobbyException;
 import netcracker.hobbies.*;
+import org.apache.log4j.Logger;
 
 public class App {
+    final static Logger logger = Logger.getLogger(App.class);
+
     public static void main(String[] args) {
         try {
             Swimming swim = new Swimming("брасс", 6f, (byte) 11, true, -40f);
             swim.tellAboutHobby();
         } catch (HobbyException e) {
-            System.out.println(e.getMessage());
+            logger.error(e.getMessage());
         }
     }
 }
